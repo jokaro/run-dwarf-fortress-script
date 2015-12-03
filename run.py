3732ad0328b4bf8ed48cda5862a01f70ea94c78b
Joakim Karlsson <jokaro@users.noreply.github.com>
2015-12-03 13:47:47 +0100

    Update run.py doc

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-#!/bin/python
+#!/usr/bin/env python
+"""Use this script to launch dfhack with PyLNP without getting "x-x: unexpected operator".
+This script will not lock PyLNP and allow you to launch utlities like Dwarf Therapist.
+
+Add this script to the df_linux folder.
+In PyLNP open File -> Configure terminal... and add the following in the text box.
+
+python run.py $
+
+:copyright: (c) 2015 by Joakim Karlsson.
+:license: MIT, see LICENSE for more details.
+"""
 
 import sys
 import subprocess
